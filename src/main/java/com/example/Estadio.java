ed3c331cbc28ba633772a1bad1d63b87998e4101
pedro-2493 <pedrozamora1993@gmail.com>
2025-07-15 10:13:21 -0500

    clase estadio

---
+++ src/main/java/com/example/Estadio.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Estadio {
+
+    String nombre;
+    String ciudad;
+    int capacidad;
+
+    void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Ciudad: " + ciudad);
+        System.out.println("Capacidad: " + capacidad);
+    }
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,19 @@
 package com.example;
 
 public class Main {
+
     public static void main(String[] args) {
 
-       
+        Estadio estadio1 = new Estadio();
+        Estadio estadio2 = new Estadio();
+        estadio1.nombre = "Atanasio Girardot";
+        estadio1.ciudad = "Medellín";
+        estadio1.capacidad = 45000;
+        estadio2.nombre = "El Campín";
+        estadio2.ciudad = "Bogotá";
+        estadio2.capacidad = 36000;
+        estadio1.mostrarInformacion();
+        estadio2.mostrarInformacion();
+
     }
 }
-
